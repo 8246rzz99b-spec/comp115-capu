10c11091ff287ecfe261af237bed790e04054443
Barsin Saki <8246rzz99b@privaterelay.appleid.com>
2026-01-08 13:55:04 -0800

    Add files via upload

---
+++ lab1_Barsin.py
@@ -0,0 +1,41 @@
+#Exercise 1: modifying "Hello, World!" to "Hello, ALice!"
+
+print("Hello, Alice!")
+
+#-----------------------------------------------------------------------
+
+#Exercise 2: syntax errorPytho
+
+print(2 + 3)
+
+#The syntax error was that the the closing bracket was not putted
+
+#-----------------------------------------------------------------------
+
+#Exercise 3: Semantic error
+
+print("The area of a rectangle with length 4 and width 3 is,", 3 * 4)
+
+#The semantic error was that the numbers were added together 
+#but for area the numbers should be multiplied like Length * Width
+
+#-----------------------------------------------------------------------
+print("")
+#Exercise 4: Letter to my Professor
+
+prof_name = "ALice"
+my_name = "Barsin"
+hobby = "working out"
+coding_ex = "advanced"
+weekly_hours_learn_coding = 8
+my_github_comp115_repo = "https://github.com/8246rzz99b-spec/comp115-capu"
+
+print(f"Hi Professor {prof_name},")
+print(f"I am {my_name}, and I love {hobby} in my free time")
+print(f"My coding experience is {coding_ex}, at least I think.")
+print(f"I would love to learn coding {weekly_hours_learn_coding} hours per week")
+print(f"Here is my COMP115 repo at GitHub: {my_github_comp115_repo}")
+print("")
+print("Hope we will have fun in learning Python together this term!")
+print("Cheers,")
+print(my_name)
